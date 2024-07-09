b09f2f3b07bde8cff4942f3dbcc2261a8f68c9de
Elkarroudi <amineelkarroudi04@proton.me>
2024-07-09 09:55:43 +0100

    Base Service

---
+++ sourceCode/backEnd/app/Services/BaseService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class BaseService
+{
+    protected function response($status, $statusName, $data): JsonResponse
+    {
+        return response()->json([
+            'status' => $status,
+            $statusName => $data
+        ]);
+    }
+
+    protected function incorrectHttpMethod(): JsonResponse
+    { return $this->response(false, 'errors', '(Incorrect Http Method), This HTTP Method Is Not Supported By This Route !!'); }
+
+    protected function responseWithErrors($errors): JsonResponse
+    { return $this->response(false, 'errors', $errors); }
+
+    protected function responseWithSuccess($data): JsonResponse
+    { return $this->response(true, 'data', $data); }
+
+    public function storePicture(Request $request, $fileName, )
+    { return $request->file($fileName)->store(); }
+}
